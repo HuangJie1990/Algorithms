ee78362f996aa785675416259f80663c304e643d
huangjie <huangjie2000@163.com>
2018-01-14 20:48:26 +0800

    Selection排序算法成本分析

--- src/c2/Selection.java
+++ src/c2/Selection.java
@@ -6,14 +6,18 @@ package c2;
  **/
 public class Selection {
 
-    private Selection(){}
+    private Selection() {
+    }
 
     public static void sort(Comparable[] a) {
-        for (int i = 0; i < a.length; i++) {
+        int N=a.length;
+        for (int i = 0; i < N; i++) {
             int min = i;
-            for (int j = i; j < a.length; j++) {
+            //比较N-j次，共比较（N-1)N/2次
+            for (int j = i + 1; j < N; j++) {
                 if (Sort.less(a[j], a[min])) min = j;
             }
+            //每次外循环交换一次，最后一次循环最后一个元素自交换，共交换N次
             Sort.exch(a, i, min);
             assert Sort.isSorted(a, 0, i);
         }
