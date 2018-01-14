4ec0717dd6a496963e2a07370c004ee9fb77015e
huangjie <huangjie2000@163.com>
2018-01-14 20:48:57 +0800

    优先队列测试用例

--- src/c2/TopM.java
+++ src/c2/TopM.java
@@ -1,18 +1,33 @@
 package c2;
 
-import edu.princeton.cs.algs4.Date;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Transaction;
 
 public class TopM {
     public static void main(String[] args) {
-        In in = new In(args[0]);
-        int m = in.readInt();
-        PriorityQueue<Transaction> pq = new MinPQ();
+        int M=Integer.parseInt(args[0]);
+        In in = new In(args[1]);
+        PriorityQueue<Transaction> pq = new MaxPQ<>(0);
+        StdOut.println(pq.max());
+        StdOut.println(pq.delMax());
         while (!in.isEmpty()) {
             String t = in.readLine();
             Transaction transaction = new Transaction(t);
-
+            pq.insert(transaction);
+//            if (pq.size() > M) pq.delMin();
+        }
+        for (Transaction t :
+                pq) {
+            StdOut.println(t);
+        }
+        StdOut.println("------------------------------------------");
+        Stack<Transaction> stack = new Stack<>();
+        while (!pq.isEmpty()) stack.push(pq.delMax());
+        for (Transaction t :
+                stack) {
+            StdOut.println(t);
         }
     }
 }
