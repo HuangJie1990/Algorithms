8f169fec192ce4529d8b81e7b6c5d099c3e7c988
huangjie <huangjie2000@163.com>
2018-01-14 20:46:34 +0800

    利用Merge排序算法计算逆序数目（nlog2n)

--- src/c2/Merge.java
+++ src/c2/Merge.java
@@ -6,8 +6,7 @@ package c2;
  **/
 public class Merge {
 
-    public static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
-
+        public static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
         assert Sort.isSorted(a, lo, mid);
         assert Sort.isSorted(a, mid + 1, hi);
         for (int i = lo; i <= hi; i++) {
@@ -23,11 +22,6 @@ public class Merge {
         assert Sort.isSorted(a, lo, hi);
     }
 
-    public static void merge(Comparable[] a, Comparable[] aux, int lo, int hi) {
-        int mid = lo + (hi - lo) / 2;
-        merge(a, aux, lo, mid, hi);
-    }
-
     public static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
         if (lo >= hi) return;
         int mid = lo + (hi - lo) / 2;
@@ -40,5 +34,34 @@ public class Merge {
         int n = a.length;
         Comparable[] aux = new Comparable[n];
         sort(a, aux, 0, n - 1);
+    }
+
+    //计算逆序数
+    public static int inversion(Comparable[] a){
+        int n = a.length;
+        Comparable[] aux = new Comparable[n];
+        return inversion(a, aux, 0, n - 1);
+    }
+
+    private static int inversion(Comparable[] a, Comparable[] aux, int lo, int hi) {
+        int n=0;
+        if (lo >= hi) return 0;
+        int mid = lo + (hi - lo) / 2;
+        n+=inversion(a,aux,lo,mid);
+        n+=inversion(a,aux,mid+1,hi);
+        for (int i = lo; i <= hi; i++) {
+            aux[i]=a[i];
+        }
+        int i=lo,j=mid+1;
+        for (int k = lo; k <=hi ; k++) {
+            if (i > mid) a[k] = aux[j++];
+            else if (j > hi) a[k] = aux[i++];
+            else if (Sort.less(aux[j], aux[i])){
+                a[k] = aux[j++];
+                n+=mid-i+1;
+            }
+            else a[k] = aux[i++];
+        }
+        return n;
     }
 }
