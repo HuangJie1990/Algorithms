f5c98c6a0041ee51110ae73ced4e1d9d365ef55c
huangjie <huangjie2000@163.com>
2018-02-26 20:39:22 +0800

    添加week 8作业

---
+++ src/w8/SeamCarver.java
@@ -0,0 +1,57 @@
+package w8;
+
+import edu.princeton.cs.algs4.Picture;
+
+/**
+ * @author huangjie
+ * @create 2018-02-26-18:19
+ **/
+public class SeamCarver {
+    private Picture picture;
+
+    // create a seam carver object based on the given picture
+    public SeamCarver(Picture picture) {
+        this.picture = new Picture(picture);
+    }
+
+    // current picture
+    public Picture picture() {
+        return picture;
+    }
+
+    // width of current picture
+    public int width() {
+        return picture.width();
+    }
+
+    // height of current picture
+    public int height() {
+        return picture.height();
+    }
+
+    // energy of pixel at column x and row y
+    public double energy(int x, int y) {
+        return 0.0;
+    }
+
+    // sequence of indices for horizontal seam
+    public int[] findHorizontalSeam() {
+        return null;
+    }
+
+    // sequence of indices for vertical seam
+    public int[] findVerticalSeam() {
+        return null;
+    }
+
+    // remove horizontal seam from current picture
+    public void removeHorizontalSeam(int[] seam) {
+
+    }
+
+    // remove vertical seam from current picture
+    public void removeVerticalSeam(int[] seam) {
+
+    }
+
+}
